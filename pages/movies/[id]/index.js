8e0d58fe771a2b1de2a0b66e1a50fc2ba470b6b5
ricky <rjbenavides@cpp.edu>
2020-12-06 17:12:20 -0800

    Changed dynamic movie page

--- pages/movies/[id]/index.js
+++ pages/movies/[id]/index.js
@@ -1,6 +1,16 @@
-import { getMovieById, getMovies } from "../.././actions";
+import { useRouter } from "next/router";
+
+import { getMovieById, getMovies, deleteMovie } from "../../../actions";
 
 const Movie = ({ movie }) => {
+  const router = useRouter();
+
+  const handleDeleteMovie = (id) => {
+    deleteMovie(id).then(() => {
+      router.push("/");
+    });
+  };
+
   return (
     <div className="container">
       <div className="jumbotron">
@@ -8,6 +18,12 @@ const Movie = ({ movie }) => {
         <p className="lead">{movie.description}</p>
         <hr className="my-4" />
         <p>{movie.genres.map((g) => g + " ")}</p>
+        <button
+          onClick={() => handleDeleteMovie(movie.id)}
+          className="btn btn-danger btn-lg"
+        >
+          Delete
+        </button>
       </div>
       <p className="desc-text" style={{ fontSize: "21px" }}>
         {movie.longDesc}
